29177fe2a79dbfae57a932f77cc4256cb56cbce7
xuan1106 <spu96508@gmail.com>
2021-08-15 11:59:23 +0800

    feat: add handlebars helpers and method override

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const methodOverride = require('method-override')
+const hbsHelpers = require('handlebars-helpers')
+const multiHelpers = hbsHelpers()
 const routes = require('./routes')
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -11,11 +14,11 @@ const PORT = process.env.PORT
 const app = express()
 
 
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs', helpers: multiHelpers }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
-
+app.use(methodOverride('_method'))
 
 app.use(routes)
 
