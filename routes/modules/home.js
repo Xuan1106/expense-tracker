90da75709a8930497e4a57a576f36ee459861211
xuan1106 <spu96508@gmail.com>
2021-08-15 11:11:45 +0800

    feat: complete index page

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,10 +2,17 @@ const express = require('express')
 const router = express.Router()
 const Record = require('../../models/record')
 const Category = require('../../models/category')
+const { dateToString } = require('../../public/javaScript/tools')
 
 
 router.get('/', (req, res) => {
-  res.render('index')
+ return Record.find()
+    .lean()
+    .then(records => {
+      records.forEach(record => record.date = dateToString(record.date))
+      res.render('index', { records })
+    })
+    .catch(err => console.error(err))
 })
 
 module.exports = router
