d39727e4d278a588ed0961dc2d36faea7caec03a
xuan1106 <spu96508@gmail.com>
2021-09-07 22:22:40 +0800

    feat: add bcrypt, express-session, connect-flash, passport-local, and passport-facebook libraries

--- app.js
+++ app.js
@@ -3,7 +3,11 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const hbsHelpers = require('handlebars-helpers')
 const multiHelpers = hbsHelpers()
+const session = require('express-session')
+const flash = require('connect-flash')
+const usePassport = require('./config/passport')
 const routes = require('./routes')
+
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
 }
@@ -21,6 +25,21 @@ app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(flash())
+usePassport(app)
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.isAuthenticated()
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 app.use(routes)
 
 app.listen(PORT, () => {
---
+++ middleware/auth.js
@@ -0,0 +1,9 @@
+module.exports = {
+  authenticator: (req, res, next) => {
+    if (req.isAuthenticated()) {
+      return next()
+    }
+    req.flash('warning_msg', '請先登入才能使用網頁功能！')
+    res.redirect('/users/login')
+  }
+}
